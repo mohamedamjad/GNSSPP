6279e3f0c055827410bc13afc878474001b67985
Mohamed-Amjad LASRI <amjad.lasri@kylia.com>
2016-02-29 11:27:03 +0100

    commencer les IONEX

---
+++ include/Rinex_I.h
@@ -0,0 +1,64 @@
+#ifndef RINEX_I_H
+#define RINEX_I_H
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <math.h>
+
+class Rinex_I{
+    private:
+        struct header{
+            std::string version;
+            std::string type;
+            std::string pgm;
+            std::string run_by;
+            std::string date;
+            std::string description;
+            typedef struct epoch_first_map{
+                short int year;
+                short int month;
+                short int day;
+                short int hour;
+                short int minute;
+                short int seconde;
+            }epoch_first_map;
+
+            typedef struct epoch_last_map{
+                short int year;
+                short int month;
+                short int day;
+                short int hour;
+                short int minute;
+                short int seconde;
+            }epoch_last_map;
+            int interval;
+            int maps_number;
+            std::string mapping_function;
+            float elevation_cutoff;
+            std::string observables_used;
+            short int number_of_stations;
+            short int number_of_satellites;
+            float base_radius;
+            short int map_dimension;
+            float HGT1;
+            float HGT2;
+            float DHGT;
+            float LAT1;
+            float LAT2;
+            float DLAT;
+            float LON1;
+            float LON2;
+            float DLON;
+            int exponent;
+            };
+
+
+    public:
+
+
+
+};
+#endif
--- include/common_structures.h
+++
@@ -1,2 +0,0 @@
-struct epoch;
-struct header;
