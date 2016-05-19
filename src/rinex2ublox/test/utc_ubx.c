40f0e0ee4095bb057ccc0c406c343a9fe4be5eae
Mohamed-Amjad LASRI <amjad.lasri@kylia.com>
2016-05-19 13:19:12 +0200

    probleme restant dans les checksums

--- src/rinex2ublox/test/utc_ubx.c
+++ src/rinex2ublox/test/utc_ubx.c
@@ -8,14 +8,14 @@
 typedef struct ubxmsg{
   unsigned char id[2];
   unsigned char length[2];
-  void *payload;
+  unsigned char *payload;
   unsigned char checksum_a;
   unsigned char checksum_b;
 }ubxmsg;
 
 typedef struct nav_timeutc{
  unsigned short int sec, minute, hour, day, month, year, ToW, time_accuracy, nanosec_of_sec, validity_flag;
-};
+}nav_timeutc;
 
 typedef struct t_gps{
     int week;
@@ -54,11 +54,18 @@ void getGPStime(t_gps *gps_time, int leap_seconds, int year, int mon, int day, i
   gps_time->sec = seconds % SECONDS_IN_WEEK;
 }
 
-
 void getChecksum(ubxmsg *ptr_ubx, unsigned char *checksum_a, unsigned char *checksum_b){
+  // probleme dans cette fonction: les checksum sont variables
   // Algorithme de Fletcher: voir page 86 de u-blox6 receiver description protocol
   unsigned short int length = (ptr_ubx->length[1]<<8)|ptr_ubx->length[0];
-  
+  *checksum_a = *checksum_a + ptr_ubx->id[0];
+  *checksum_b = *checksum_a + *checksum_b;
+  *checksum_a = *checksum_a + ptr_ubx->id[1];
+  *checksum_b = *checksum_a + *checksum_b;
+  *checksum_a = *checksum_a + ptr_ubx->length[1];
+  *checksum_b = *checksum_a + *checksum_b;
+  *checksum_a = *checksum_a + ptr_ubx->length[0];
+  *checksum_b = *checksum_a + *checksum_b;
   for(int i; i<length; i++){
     *checksum_a = *checksum_a + ptr_ubx->payload[i];
     *checksum_b = *checksum_a + *checksum_b;
@@ -68,36 +75,33 @@ void getChecksum(ubxmsg *ptr_ubx, unsigned char *checksum_a, unsigned char *chec
 
 void getUTCUBX(ubxmsg *utc_msg, int leap_seconds, int year, int month, int day, int hour, int minute, int sec){
   t_gps gps_time;
-  utc_t *utc_t;
+  nav_timeutc *utc_t;
 
   utc_msg->id[0] = 0x01;
   utc_msg->id[1] = 0x21;
   utc_msg->length[0] = 0x14;
   utc_msg->length[1] = 0x00;
-  utc_msg->payload = (unsigned char *)malloc(20*sizeof(unsigned char));
-  
+  // Ajout joe 
   utc_t = malloc(sizeof(utc_t));
   utc_msg->payload = (unsigned char*)utc_t;
-  utc_t->s = gps 
-
-  utc_msg->payload = (unsigned char *)malloc(20*sizeof(unsigned char));
 
   // GPS Millis time of week
   getGPStime(&gps_time, leap_seconds, year, month, day, hour, minute, sec);
   gps_time.sec *= 1000;
+  utc_msg->payload[0] = gps_time.sec;
   memcpy(&utc_msg->payload[0], &gps_time.sec, sizeof(int));
 
   // Time Accuracy estimate
-  utc_msg->payload[4] = 0x00;
+  utc_msg->payload[4] = 0x10;
   utc_msg->payload[5] = 0x00;
   utc_msg->payload[6] = 0x00;
   utc_msg->payload[7] = 0x00;
   
   // Nanoseconds of second
-  utc_msg->payload[8] = 0x00;
-  utc_msg->payload[9] = 0x00;
-  utc_msg->payload[10] = 0x00;
-  utc_msg->payload[11] = 0x00;
+  utc_msg->payload[8] = 0x91;
+  utc_msg->payload[9] = 0xf7;
+  utc_msg->payload[10] = 0xfb;
+  utc_msg->payload[11] = 0xff;
 
   // year
   memcpy(&utc_msg->payload[12], &year, sizeof(unsigned short));
@@ -117,19 +121,23 @@ void getUTCUBX(ubxmsg *utc_msg, int leap_seconds, int year, int month, int day, 
   // seconds of minute
   memcpy(&utc_msg->payload[18], &sec, sizeof(unsigned char));
 
+  // valid flag(toujours mettre à 7 lorsque les données proviennent du rinex)
+  utc_msg->payload[19] = 0x07;
+
   // Compute checksums
   getChecksum(utc_msg, &utc_msg->checksum_a, &utc_msg->checksum_b);
 }
 
 int main(){
   ubxmsg utc_msg;
-  //b5 62 01 21 14 00 60 5c  29 0b 10 00 00 00 91 f7 fb ff e0 07 05 0a 04 00  2b 07 e4 d4
+  //b5 62 01 21 14 00 60 5c 29 0b 10 00 00 00 91 f7 fb ff e0 07 05 0a 04 00 2b 07 e4 d4
   getUTCUBX(&utc_msg, 17, 2016, 5, 10, 4, 0, 43);
   printf("\n%02X%02X%02X%02X%02X%02X",utc_msg.id[0],utc_msg.id[1], utc_msg.length[0], utc_msg.length[1], utc_msg.payload[0],utc_msg.payload[1]);
   printf("\n\n%u", (utc_msg.length[1]<<8)|utc_msg.length[0]);
   for(int i=0; i < ((utc_msg.length[1]<<8)|utc_msg.length[0]); i++){
     printf("%u:%02X ",i,utc_msg.payload[i]);
   }
+  printf("\n:checksum:%02X %02X\n", utc_msg.checksum_a, utc_msg.checksum_b);
   printf("\n");
   return 0;
 }
